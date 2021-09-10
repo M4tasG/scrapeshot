eb881b08da4696723a2841cc1c980082d5efd7a7
M4tasG <grybauskas.matas@gmail.com>
2021-09-10 10:39:11 +0300

    moved image saving to a separate function, added matching with template image function

--- func.py
+++ func.py
@@ -1,5 +1,8 @@
 import string
 import random
+import os
+import cv2
+
 
 def get_link():
     letters = string.ascii_letters
@@ -8,3 +11,20 @@ def get_link():
     let_2 = random.choice(letters).lower()
     num = random.randint(1000, 9999)
     return f'https://prnt.sc/{let_1}{let_2}{num}'
+
+def save_image(save_folder, url, img):
+    with open(os.path.join(save_folder, os.path.basename(url)), 'wb') as f:
+        f.write(img.content)
+
+
+#Unused for now, filtering no image found by imgur links
+def match_with_template(template, img):
+    try:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    except:
+        pass
+
+    result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    return max_val
